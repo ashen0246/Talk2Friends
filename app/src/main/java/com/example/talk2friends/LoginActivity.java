e343134c648cb61a97bc03832bd890d8ae2d92ce
Andy Shen <alshen@usc.edu>
2023-10-19 04:22:29 -0700

    Created email signin process

--- app/src/main/java/com/example/talk2friends/LoginActivity.java
+++ app/src/main/java/com/example/talk2friends/LoginActivity.java
@@ -2,15 +2,28 @@ package com.example.talk2friends;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
     boolean login = true;
+    boolean verifyEmailStep = false;
+    EditText email;
+    EditText password;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,31 +31,87 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_page);
         //No rotation of screen
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        email = findViewById(R.id.enterEmail);
+        password =  findViewById(R.id.enterPassword);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onClickToggleLogin(View view) {
+        verifyEmailStep = false;
         login = !login;
         Button button = findViewById(R.id.toggleLoginButton);
-        if (login){
+        TextView textView = findViewById(R.id.loginLabel);
+        Button b2 = findViewById(R.id.nextPageButton);
+        if (login) {
             button.setText(getString(R.string.signup_button));
-        }
-        else{
+            textView.setText(getString(R.string.login));
+            b2.setText(getString(R.string.next_page_button));
+        } else {
             button.setText(getString(R.string.login_button));
+            textView.setText(getString(R.string.signup));
+            b2.setText(getString(R.string.verifyEmail));
         }
 
     }
     public void onClickNextPage(View view) {
-        if (login){
-            //login
-            Intent mainPageIntent = new Intent(this, MeetingsActivity.class);
-            mainPageIntent.putExtra("email", "a");
-            startActivity(mainPageIntent);
+        if (!verifyEmailStep) {
+            if (login) {
+                //login
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    goToMeetingPage();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                }
+                            }
+                        });
+            } else {
+                //signup
+
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    mAuth.getCurrentUser().sendEmailVerification()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        Button button = findViewById(R.id.nextPageButton);
+                                                        button.setText(getString(R.string.EmailVerified));
+                                                        verifyEmailStep = true;
+                                                    }
+                                                }
+                                            });
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                }
+                            }
+                        });
+            }
         }
         else{
-            //signup
-            Intent emailVerificationIntent = new Intent(this, EmailVerificationActivity.class);
-            startActivity(emailVerificationIntent);
+            mAuth.getCurrentUser().reload();
+            if(mAuth.getCurrentUser().isEmailVerified()){
+                goToCreateProfilePage();
+            }
         }
-
+    }
+    public void goToCreateProfilePage(){
+        Intent emailVerificationIntent = new Intent(this, EmailVerificationActivity.class);
+        emailVerificationIntent.putExtra("email", email.getText());
+        startActivity(emailVerificationIntent);
+    }
+    public void goToMeetingPage(){
+        //if not in main database fo to create profile page
+        Intent mainPageIntent = new Intent(this, MeetingsActivity.class);
+        mainPageIntent.putExtra("email", email.getText());
+        startActivity(mainPageIntent);
     }
 }
